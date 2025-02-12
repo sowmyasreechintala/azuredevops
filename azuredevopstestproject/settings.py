364c6b9f96d4f6290f65233f0002a7797d8158c7
Sowmya <sowmya@Administrators-MacBook-Pro.local>
2025-02-13 01:22:10 +0530

    aa

--- azuredevopstestproject/settings.py
+++ azuredevopstestproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@vm0k$_zh8*mr&x%smv92kaiqi8_bu)crax(r#rqf)a-#*lomy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://westusapp-e4fghaffckdvbtav.westus-01.azurewebsites.net/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['westusapp-e4fghaffckdvbtav.westus-01.azurewebsites.net', 'localhost', '127.0.0.1']
 
 
 
